a4e6f1bf35958db1eed3ebbc120268663e6bd536
Tyreke Mclean <tyrekemclean@gmail.com>
2020-10-11 18:54:58 -0500

    tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,16 +1,19 @@
-"use strict"
+
 document.addEventListener('DOMContentLoaded', ()  => {
   const gameBoard = document.getElementById("board").children;
   const squares=document.querySelectorAll("board");
   const xs= "X";
   const os="O";
   const statuss=document.getElementById("status");
+  const button=document.getElementsByClassName("btn")[0];
   const winFactors=[[0,4,8],[0,3,6],[0,1,2],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[2,4,6]];
   let clicked;
   let turn=true;
   
   var i;
-  var plays=["","","","","","","","",""];
+ 
+
+  
 
   for(i=0;i<gameBoard.length;i++){
     gameBoard[i].setAttribute("class","square");
@@ -18,12 +21,15 @@ document.addEventListener('DOMContentLoaded', ()  => {
     gameBoard[i].addEventListener('mouseenter',hoverStyle);
     gameBoard[i].addEventListener('mouseleave',hoverRevert);
     gameBoard[i].addEventListener('click',checkWinner);
+     
   } 
   
   function clicks(event) {
-    var index=plays.indexOf(event.target);
+    var plays= Array.from(squares);
+   var index = plays.indexOf(event.target)
+   console.log(plays);
     
-    if(turn==true || clicked==os){
+    if(turn==true || clicked==os ){
       event.target.innerHTML= xs;
       event.target.classList.add("X");
       clicked= xs;
@@ -35,7 +41,8 @@ document.addEventListener('DOMContentLoaded', ()  => {
       clicked=os;
       turn=true;
     }  
-    console.log(plays);
+    
+    
   }
 
   function hoverStyle(event){
@@ -59,4 +66,17 @@ document.addEventListener('DOMContentLoaded', ()  => {
       }
     }
   }
+ 
+  function restart(){
+    for(i=0;i<gameBoard.length;i++){
+    gameBoard[i].innerHTML = '';
+    gameBoard[i].classList.remove("X");
+    gameBoard[i].classList.remove("O");
+    
+    statuss.classList.remove("you-won");
+    statuss.innerHTML = "Move your mouse over a square and click to play an X or an O";
+    
+     } 
+  }
+  button.addEventListener('click',restart);
 })
